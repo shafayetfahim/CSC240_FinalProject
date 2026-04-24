072ec5073d49b8231c88a62f2005060c68824516
Mahathir_Khan <63616361+Mahathir-Khan@users.noreply.github.com>
2026-04-24 10:19:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 # CSC240
 # Aiden Hughes, Mahathir Khan, Shafayet Fahim
 
-API_KEY = "uSGMJHoBaBJrzRjDLbOjslezZ8cfMGauKO5noTW6"
+API_KEY = "WziTQtToFOfEcvmpwr7T8jENcyzL6AYfNTolxFh4"
 BASE_URL = "https://api.congress.gov/v3"
